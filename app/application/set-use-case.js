1a0df10cf9b7f4b4aa5fa43b76c9de2fd9632dae
João Pedro <pedro.abreu@loldesign.com.br>
2024-03-27 17:58:44 -0300

    add logs

--- app/application/set-use-case.js
+++ app/application/set-use-case.js
@@ -1,6 +1,7 @@
 const repoInstance = require("../infrastructure/factories/in-memory-repository-factory");
 
 function setData(key, arg) {
+  console.log("arg on setData use case", arg);
   const dataRepository = repoInstance;
 
   dataRepository.save(key, arg);
