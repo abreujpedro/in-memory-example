7a16a1b3b5e23fd8b3e4b9f61f685e47f8674a66
João Pedro <pedro.abreu@loldesign.com.br>
2024-03-27 22:04:08 -0300

    core: improve read me

--- app/main.js
+++ app/main.js
@@ -8,8 +8,6 @@ const {
 // You can use print statements as follows for debugging, they'll be visible when running tests
 // console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-
 const args = process.argv;
 
 const portIndex = args.indexOf("--port");
